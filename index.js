772e19639c0b5cb0bf152e8997438b9d937a21e0
André Cruz <andremiguelcruz@msn.com>
2020-01-17 10:25:41 +0000

    fix: integration with Next.js 9.2 (#2)
    Fix for Next.js >= 9.2, where it introduced a oneOf rules for CSS
    When there are nested oneOf rules, Webpack stops at the first occurrence of a `oneOf` rule because it has no conditions (test, exclude, etc).
    We fix that by adding a test condition to the oneOf rule

--- index.js
+++ index.js
@@ -3,6 +3,19 @@
 module.exports = (nextConfig = {}) => ({
     ...nextConfig,
     webpack(config, options) {
+        // Fix for Next.js >= 9.2, where it introduced a oneOf rules for CSS
+        // When there are nested oneOf rules, Webpack stops at the first occurrence of a `oneOf` rule
+        // because it has no conditions (test, exclude, etc).
+        // We fix that by adding a test condition to the CSS oneOf rule
+        const cssRule = config.module.rules.find((rule) => (
+            rule.oneOf && Object.keys(rule).length === 1) &&
+            rule.oneOf.some((rule) => `${rule.test}`.includes('\\.module\\.css')),
+        );
+
+        if (cssRule) {
+            cssRule.test = /\.css$/;
+        }
+
         config.module.rules = [{
             oneOf: [...config.module.rules],
         }];
