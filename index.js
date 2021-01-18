7ac886f5d0d4c883e735e3cd54384b929b6231f3
André Cruz <andremiguelcruz@msn.com>
2021-01-18 04:20:46 +0000

    feat: next 10 compat
    Also update deps.

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 module.exports = (nextConfig = {}) => ({
     ...nextConfig,
     webpack(config, options) {
-        // Fix for Next.js >= 9.2, where it introduced a oneOf rules for CSS
+        // Fix for Next.js >= 9.2, where it introduced a oneOf rules for CSS.
         // When there are nested oneOf rules, Webpack stops at the first occurrence of a `oneOf` rule
         // because it has no conditions (test, exclude, etc).
-        // We fix that by adding a test condition to the CSS oneOf rule
+        // We fix that by adding a test condition to the CSS oneOf rule.
         const cssRule = config.module.rules.find((rule) => (
             rule.oneOf && Object.keys(rule).length === 1) &&
             rule.oneOf.some((rule) => `${rule.test}`.includes('\\.module\\.css')),
